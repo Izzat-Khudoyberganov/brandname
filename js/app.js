1c2c9adfcaaa35a56761271dc972f0dbdb0e11be
izzat <izzatdesign001@gmail.com>
2025-01-27 16:26:38 +0500

    added loader and dialog components

--- js/app.js
+++ js/app.js
@@ -15,3 +15,34 @@ burgerMenu.addEventListener("click", () => {
 
 AOS.init();
 
+// loader
+setTimeout(() => {
+  console.log("Time is out");
+  document.querySelector("#loader").style.display="none"
+  document.querySelector("header").style.display="block"
+  document.querySelector("main").style.display="block"
+  document.querySelector("footer").style.display="block"
+}, 3000)
+
+// DIALOG
+const modal = document.querySelector(".login-modal")
+const showModal = document.querySelector(".login-btn")
+const closeModal = document.querySelector(".close-modal-btn")
+const loginSubmitBtn = document.querySelector(".login-form_button")
+const loginForm = document.querySelector(".login-form")
+showModal.addEventListener("click", () => {
+  modal.showModal()
+})
+
+closeModal.addEventListener("click", () => {
+  modal.close()
+})
+
+
+loginSubmitBtn.addEventListener("click", (e)=> {
+  e.preventDefault()
+  loginForm.reset()
+  modal.close()
+  
+})
+
