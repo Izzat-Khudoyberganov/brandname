3c6d5a8718414e778da4f07be9706962bcd8aa5c
izzat <izzatdesign001@gmail.com>
2025-02-05 15:11:45 +0500

    added smth

--- js/app.js
+++ js/app.js
@@ -1,48 +1,40 @@
 const burgerMenu = document.querySelector(".burger-menu");
-const nav = document.querySelector(".header-nav")
+const nav = document.querySelector(".header-nav");
 
 burgerMenu.addEventListener("click", () => {
   console.log("Clicked");
-  // if (nav.classList.contains("active")) {
-  //   nav.classList.remove("active")
-  // } else {
-  //   nav.classList.add("active")
-  // }
-  nav.classList.toggle("active")
-  burgerMenu.classList.toggle("active")
+  nav.classList.toggle("active");
+  burgerMenu.classList.toggle("active");
 });
-
 
 AOS.init();
 
 // loader
 setTimeout(() => {
   console.log("Time is out");
-  document.querySelector("#loader").style.display="none"
-  document.querySelector("header").style.display="block"
-  document.querySelector("main").style.display="block"
-  document.querySelector("footer").style.display="block"
-}, 3000)
+  document.querySelector("#loader").style.display = "none";
+  document.querySelector("header").style.display = "block";
+  document.querySelector("main").style.display = "block";
+  document.querySelector("footer").style.display = "block";
+}, 3000);
 
 // DIALOG
-const modal = document.querySelector(".login-modal")
-const showModal = document.querySelector(".login-btn")
-const closeModal = document.querySelector(".close-modal-btn")
-const loginSubmitBtn = document.querySelector(".login-form_button")
-const loginForm = document.querySelector(".login-form")
+const modal = document.querySelector(".login-modal");
+const showModal = document.querySelector(".login-btn");
+const closeModal = document.querySelector(".close-modal-btn");
+const loginSubmitBtn = document.querySelector(".login-form_button");
+
+const loginForm = document.querySelector(".login-form");
 showModal.addEventListener("click", () => {
-  modal.showModal()
-})
+  modal.showModal();
+});
 
 closeModal.addEventListener("click", () => {
-  modal.close()
-})
+  modal.close();
+});
 
-
-loginSubmitBtn.addEventListener("click", (e)=> {
-  e.preventDefault()
-  loginForm.reset()
-  modal.close()
-  
-})
-
+loginSubmitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  loginForm.reset();
+  modal.close();
+});
